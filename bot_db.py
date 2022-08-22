1ebe335a7bd56e4cb7a234ebe563bef6d7b6bb6d
Василий Перов <vasiliiperov@gmail.com>
2022-08-22 20:07:53 +0700

    added SQL IF NOT EXITS and INSERT OR IGNORE statements, + cursor.close() for functions

--- bot_db.py
+++ bot_db.py
@@ -2,33 +2,28 @@ import sqlite3
 
 
 class BotDb:
-    """Думаю, разумно создать разные фунцкии на получение админов/пользователей здесь, а не реализовывать это
-    в main"""
-
-    _db = sqlite3.connect('bot_users')
 
     def __init__(self):
-        cursor = self._db.cursor()
-        try:
-            cursor.execute('''
-                CREATE TABLE tb_users(id INTEGER PRIMARY KEY, is_admin BOOLEAN)
-                ''')
-            self._db.commit()
+        self._db = sqlite3.connect('bot_users')
+        self.create_table_if_not_exists()
 
-        except sqlite3.OperationalError:
-            pass
+    def create_table_if_not_exists(self):
+        cursor = self._db.cursor()
+        cursor.execute('''
+                       CREATE TABLE IF NOT EXISTS tb_users(id INTEGER PRIMARY KEY, is_admin BOOLEAN)
+                       ''')
+        self._db.commit()
+        cursor.close()
 
     def add_to_database(self, user_id: int, is_admin: bool = False) -> None:
         cursor = self._db.cursor()
-        try:
-            cursor.execute('''
-            INSERT INTO tb_users(
-            id, is_admin)
-            VALUES(?,?)
-            ''', (user_id, is_admin))
-            self._db.commit()
-        except sqlite3.IntegrityError:
-            pass
+        cursor.execute('''
+                       INSERT OR IGNORE INTO tb_users(
+                       id, is_admin)
+                       VALUES(?,?)
+                       ''', (user_id, is_admin))
+        self._db.commit()
+        cursor.close()
 
     def get_user_ids(self) -> set:
         cursor = self._db.cursor()
@@ -37,6 +32,7 @@ class BotDb:
         all_user_ids = set()
         for row in all_rows:
             all_user_ids.add(row[0])
+        cursor.close()
         return all_user_ids
 
     def get_admin_ids(self) -> set:
@@ -47,6 +43,7 @@ class BotDb:
         for row in all_rows:
             if row[1]:
                 admin_ids.add(row[0])
+        cursor.close()
         return admin_ids
 
 
